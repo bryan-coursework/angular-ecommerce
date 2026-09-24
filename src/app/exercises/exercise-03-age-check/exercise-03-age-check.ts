47e5ae754606a632d4745185bca453653534b32b
bryan-coursework <bryanalvarenga@pm.me>
2026-09-24 15:33:24 -0300

    feat: implementando terceiro exercício

--- src/app/exercises/exercise-03-age-check/exercise-03-age-check.ts
+++ src/app/exercises/exercise-03-age-check/exercise-03-age-check.ts
@@ -6,4 +6,16 @@ import { Component } from '@angular/core';
   styleUrl: './exercise-03-age-check.scss',
   templateUrl: './exercise-03-age-check.html',
 })
-export class Exercise03AgeCheck {}
+export class Exercise03AgeCheck {
+  idade: number = 0;
+
+  aumentarIdade(): void {
+    this.idade += 1;
+  }
+
+  diminuirIdade(): void {
+    if (this.idade > 0) {
+      this.idade -= 1;
+    }
+  }
+}

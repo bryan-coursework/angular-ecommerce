7c999db4703dd3f26eb6f5471b92aa5b82c431a2
bryan-coursework <bryanalvarenga@pm.me>
2026-09-24 15:54:32 -0300

    feat: implementando sexto exercício

--- src/app/exercises/exercise-06-empty-list/exercise-06-empty-list.ts
+++ src/app/exercises/exercise-06-empty-list/exercise-06-empty-list.ts
@@ -6,4 +6,19 @@ import { Component } from '@angular/core';
   styleUrl: './exercise-06-empty-list.scss',
   templateUrl: './exercise-06-empty-list.html',
 })
-export class Exercise06EmptyList {}
+export class Exercise06EmptyList {
+  nomesIniciais: string[] = ['Ana', 'Bruno', 'Carlos', 'Daniela', 'Eduardo'];
+  nomes: string[] = [...this.nomesIniciais];
+
+  removerUltimoNome(): void {
+    this.nomes.pop();
+  }
+
+  limparLista(): void {
+    this.nomes = [];
+  }
+
+  restaurarLista(): void {
+    this.nomes = [...this.nomesIniciais];
+  }
+}
